3c3aea385c0d0240908946032fc05db75f98f5e2
maq218 <maq218@lehigh.edu>
2014-09-01 21:56:47 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,22 @@
+//Mark Quentzel
+//Welcome Class Java Program
+
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+    public static void main(String[] args) {
+
+//  print the Welcome Class statement
+    System.out.println("  -----------");
+    System.out.println("  | WELCOME |");    
+    System.out.println("  -----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^  ");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("|-M--A--Q--2--1--8-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+    System.out.println("  v  v  v  v  v  v  ");
+    System.out.println("This summer I worked as a lifeguard and a swim instructor at the JCC camp");
+    System.out.println("in Tenafly, New Jersey. I had a lot of fun and made many friends.");
+    }
+}
