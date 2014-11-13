9e0f213e2d5cbebae8b14be899ff38b83a92c2cf
maq218 <maq218@lehigh.edu>
2014-11-13 21:22:58 +0000

    lab04

--- lab04/BigMacTimeAgain.java
+++ lab04/BigMacTimeAgain.java
@@ -6,6 +6,7 @@
 //take input from user and use the data to make basic computations 
 
 import java.util.Scanner; 
+import java.text.DecimalFormat;
 
 // define a class 
 public class BigMacTimeAgain{
@@ -14,38 +15,41 @@ public class BigMacTimeAgain{
     public static void main(String[] args) {
 
         Scanner myScanner = new Scanner(System.in);
+        DecimalFormat d = new DecimalFormat("00.00");
         //creates variables for what user puts in scanner
         
         System.out.print("Enter the number of Big Macs: ");
-        int nBigMacs = myScanner.nextInt();
-        if(nBigMacs>0){
-                System.out.println("You ordered " + nBigMacs + " Big Macs for a cost of " + nBigMacs + "x2.22 = $" + nBigMacs*2.2);
-        }
-        else if (nBigMacs<=0){
-                System.out.println("You did not enter an int > 0");
-                return;
+        if(myScanner.hasNextInt()){
+                int nBigMacs = myScanner.nextInt();
+                if(nBigMacs>0){
+                        System.out.println("You ordered " + nBigMacs + " Big Macs for a cost of " + nBigMacs + "x2.22 = $" + nBigMacs*2.2);
+                        System.out.print("Do you want an order of fries (Y/y/N/n)?: ");
+                        Scanner scan = new Scanner(System.in);
+                        String fries = scan.next();
+                        
+                        if(fries.equals("Y") || fries.equals("y")){
+                                System.out.println("You ordered fries at a cost of $2.15");
+                                System.out.println("The total cost of the meal is " + d.format(nBigMacs*2.22 + 2.15));
+                                
+                        }
+                        else if (fries.equals("N") || fries.equals("n")){
+                                System.out.println("The total cost of the meal is " + d.format(nBigMacs*2.22));
+                        }
+                        
+                        else{
+                                System.out.println("You did not enter one of 'y', 'Y', 'N', 'n'");
+                        }
+                }
+                else if (nBigMacs<=0){
+                        System.out.println("You did not enter an int > 0");
+                        return;
+                }
         }
         else{ 
                 System.out.println("You did not enter an int");
                 return; //leaves the program, i.e. the program terminates
-             }
-        
-        
-        System.out.print("Do you want an order of fries (Y/y/N/n)?: ");
-        String fries = myScanner.next();
-        
-        if(fries == "Y" || fries == "y"){
-                System.out.println("You ordered fries at a cost of $2.15");
-                System.out.println("The total cost of the meal is" + (nBigMacs*2.22 + 2.15));
-                
         }
-        else if (fries == "N" || fries == "n"){
-                System.out.println("The total cost of the meal is" + (nBigMacs*2.22));
-        }
-        
-        else{
-                System.out.println("You did not enter one of 'y', 'Y', 'N', 'n'");
-        }
+       
     }
     
 }
--- testfolder/Test1.java
+++ testfolder/Test1.java
@@ -2,14 +2,24 @@
 public class Test1 {
     public static void main (String [] args) {
         
-        int j = 4;
-        
-        j *= 5;
-        
-        System.out.println(j);
-        
-        System.out.println(j);
-        
-        
+   int x = 3;
+int y = 3;
+do {
+	x += 1;
+	do {
+		System.out.println( "[" + y + "]" );
+		y -= 1;
+	} while( y > 0 );
+} while(x < 5);
+
+   x = 3;
+ y = 3;
+for(x=3; x<6; x++){
+	
+	for (y=3; y>-1; y--){
+		System.out.println( "[" + y + "]" );
+	
+	} 
+} 
     }
 }
